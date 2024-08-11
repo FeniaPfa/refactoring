f49356af2d835194a83bd055cc988722e9187db5
FeniaPfa <fa.aguilarpfa@gmail.com>
2024-08-10 21:37:30 -0400

    replace play for playFor in amountFor

--- chapter-1/example/statement.js
+++ chapter-1/example/statement.js
@@ -25,10 +25,6 @@ const plays = {
 };
 
 function statement(invoice, plays) {
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
@@ -55,28 +51,32 @@ function statement(invoice, plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
-}
 
-function amountFor(aPerformance, play) {
-  let result = 0;
-  switch (play.type) {
-    case 'tragedy':
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case 'comedy':
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
-  return result;
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
 }
 
 console.log(statement(invoices[0], plays));
