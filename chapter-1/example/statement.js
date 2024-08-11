e7761a5551abfa3dd76e917869a13b342e973230
FeniaPfa <fa.aguilarpfa@gmail.com>
2024-08-10 22:12:39 -0400

    move /100 to usd function

--- chapter-1/example/statement.js
+++ chapter-1/example/statement.js
@@ -29,7 +29,7 @@ function usd(aNumber) {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
-  }).format(aNumber);
+  }).format(aNumber / 100);
 }
 
 function statement(invoice, plays) {
@@ -41,12 +41,12 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
